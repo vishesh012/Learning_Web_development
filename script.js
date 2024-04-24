337d036190a599ee40267db3e8b32bcf79ce642f
Vishesh <visheshb002bs@gmail.com>
2024-04-25 03:46:23 +0530

    Video 2 of the course completed.

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 //I WIll be Maintaing Comment History as I Make progress in The course.    Date - 25-04-2024 Time - 02:30 AM
+//alert is a function that will show a pop up message to the user.
+//we link the script.js file to the index.html file using the <script> tag.
+alert("Welcom to our website!")
