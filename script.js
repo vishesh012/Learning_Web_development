c71ab940d7784e90accf4a83f07790d41fe12705
Vishesh <visheshb002bs@gmail.com>
2024-05-07 01:27:17 +0530

    Tutorial 54 complete startef JS got Intro of Javascript and node.js

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 // Date - 25-04-2024 Time - 02:30 AM
-//alert is a function that will show a pop up message to the user.
-//we link the script.js file to the index.html file using the <script> tag.
-alert("Welcome to our website!")
+//we link the script.js file to the index.html file using the <script src> tag.
+alert("! Welcome to our website !");
+console.log("Executing");
+var num = prompt("Enter your Number");
+var isTrue = confirm("Is your Number " + num);
+
+if (isTrue) {
+  alert("Your Number is " + num);
+  console.log("Your Number is " + num);
+}
+else {
+  alert("Your Number is not " + num);
+  console.log("Your Number is not " + num);
+}
