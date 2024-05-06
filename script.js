b0cb23ad356577af1160a6d80dd8b711636816d4
Vishesh <visheshb002bs@gmail.com>
2024-05-07 02:17:32 +0530

    Tutorial 55 complete learned about variables datatypes and objects

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 // Date - 25-04-2024 Time - 02:30 AM
 //we link the script.js file to the index.html file using the <script src> tag.
-alert("! Welcome to our website !");
-console.log("Executing");
-var num = prompt("Enter your Number");
-var isTrue = confirm("Is your Number " + num);
+console.log("Hello World!");
 
-if (isTrue) {
-  alert("Your Number is " + num);
-  console.log("Your Number is " + num);
+var num1 = 5;
+let num2 = 10;
+let sum = num1 + num2;
+let name1 = "John";
+
+console.log(sum);
+console.log(typeof sum, typeof num1, typeof name1);
+
+const a1 = 6;
+// a1 = 10; can't change value because it is a constant
+
+{
+    let num1 = 13;
+    console.log(num1);
 }
-else {
-  alert("Your Number is not " + num);
-  console.log("Your Number is not " + num);
+console.log(num1);
+
+let obj ={
+    name: "John",
+    "job code": 32325, //writing Job role in quotes because it has a space in between
+    "Is Productive": true
 }
+console.log(obj);
+
+obj.salary = "12cr";
+console.log(obj);
+obj.salary = "15cr";
+console.log(obj);
