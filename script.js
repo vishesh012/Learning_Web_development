038ca55569c9609345daf20a4b7a668b0a59657e
Vishesh <visheshb002bs@gmail.com>
2024-05-07 12:57:27 +0530

    Tutorial 58 complete learned about Functions in JS

--- script.js
+++ script.js
@@ -1,38 +1,41 @@
 // Date - 25-04-2024 Time - 02:30 AM
 //we link the script.js file to the index.html file using the <script src> tag.
 console.log("Executing");
-console.log("Learning Loop");
+console.log("Learning Functions in JavaScript");
 
-let num1;
-for (num1 = 0; num1 <= 100; num1++) {
-  console.log(num1);
-}
-
-let obj = {
-  name: "Nikhil",
-  role: "Developer",
-  age: 21,
-  company: "Google",
-}
-for (const key in obj) {
-  // if (Object.hasOwnProperty.call(obj, key)) {
-  const element = obj[key];
-  console.log(key, element);
-}
-
-for (const iterator of "Vishesh") {
-  console.log(iterator);
+// Function Declaration
+function nice(name) {
+  console.log("Hello, World! " + name);
+  console.log("How are you " + name);
 
 }
 
+nice("vishesh");
 
-let i = 5;
-while (i < 10) {
-  console.log(i);
-  i++;
+function sum(a, b, c = 3) { //default value of c is 3
+  console.log(a + b)
+  return a + b + c;
 }
-let num2 = 10
-do {
-  num2++;
-  console.log(num2);
-} while (num2 < 0); //Do while loop will execute atleast once even if the condition is false.
+result = sum(2, 3);
+result2 = sum(4);//c is 3 by default a is 4 and b is undefined so b is 0 will produce NaN
+result3 = sum(2, 7, 4); //c is 4
+
+console.log("sum of these number is : ", result);
+console.log("sum of these number is : ", result2);
+console.log("sum of these number is : ", result3);
+
+
+const func1= (x)=>{
+  console.log("Hey I am an Arrow Function",x);
+}
+func1(32);
+func1(34, 2); //it will ignore the extra arguments  
+func1(84);
+func1(); //it will produce undefined
+//Arrow function does not have arguments object
+//Arrow function does not have this keyword
+//Arrow function does not have prototype property
+//Arrow function does not have super property
+//Arrow function does not have new.target property
+//Arrow function does not have call, apply, bind method
+//Arrow function does not have constructor
